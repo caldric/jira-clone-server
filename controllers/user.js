e69f9826d4704ee453f151d416ddb026d09fa829
caldric <cvaldric@gmail.com>
2020-12-14 02:45:46 -0500

    Validate user registration form information

--- controllers/user.js
+++ controllers/user.js
@@ -7,14 +7,47 @@ const User = require('../models/user.js');
 const userRouter = express.Router();
 
 // Routers
-userRouter.post('/signup', async (req, res) => {
-  // Hash password
-  req.body.password = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync());
+userRouter.post('/register', async (req, res) => {
+  // Destructure the body
+  const { email, password } = req.body;
 
-  const newUser = await User.create(req.body).catch((err) =>
+  // Validation: all fields must be present
+  if (!email || !password) {
+    return res.status(400).json({ error: 'All fields are required' });
+  }
+
+  // Validation: password must be a minimum of 8 characters
+  if (password.length < 8) {
+    return res
+      .status(400)
+      .json({ error: 'The password needs to be at least 8 characters long' });
+  }
+
+  // Validation: check if user has alredy created account
+  const existingUser = await User.findOne({ email }).catch((err) =>
     res.status(400).json({ error: err.message })
   );
-  res.status(200).json(newUser);
+
+  if (existingUser) {
+    return res
+      .status(400)
+      .json({ error: 'An account with this email already exists' });
+  }
+
+  // Hash password
+  const hashedPassword = bcrypt.hashSync(
+    req.body.password,
+    bcrypt.genSaltSync()
+  );
+
+  // Create new user in the database
+  const newUser = await User.create({
+    email,
+    password: hashedPassword,
+  }).catch((err) => res.status(400).json({ error: err.message }));
+
+  // Return newly created user information if everything is successful
+  return res.status(200).json(newUser);
 });
 
 // Export
